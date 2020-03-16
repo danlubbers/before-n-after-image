8dda8b982015d2c2860b8400425775fa73856386
Dan Lubbers <lubbers.dan@gmail.com>
2020-03-16 12:29:35 -0400

    Setup state mangement with onClick function to show before and after images overlaid atop one another

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import RetouchingExample from './Components/RetouchingExample/RetouchingExample'
 function App() {
   return (
     <div className="App">
+      <h1>Click Image to see Before and After Retouching</h1>
       <RetouchingExample />
     </div>
   );
--- src/Components/RetouchingExample/RetouchingExample.jsx
+++ src/Components/RetouchingExample/RetouchingExample.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import before from '../../assets/images/floro-cristina-5107-before.jpg';
 import after from '../../assets/images/floro-cristina-5107-final.jpg';
 
 export default function RetouchingExample() {
-  
+
+  const [toggle, setToggle] = useState(false);
+
+  const handleClick = () => {
+    console.log('test click')
+    setToggle(!toggle);
+  }
+
   return (
     <div className='image-container'>
-
-      <img className='retouching-before' src={before} alt=""/>
-      <img className='retouching-after' src={after} alt=""/>
-
+      {toggle ?
+        <img className='retouching-before' src={before} alt="" onClick={handleClick}/> :
+        <img className='retouching-after' src={after} alt="" onClick={handleClick}/>
+      }
     </div>
   )
 }
