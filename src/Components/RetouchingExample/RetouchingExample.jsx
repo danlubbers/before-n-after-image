4ae9f3a42c2b8a8bb721ea26f9c5277583bb7e13
Dan Lubbers <lubbers.dan@gmail.com>
2020-03-16 12:11:49 -0400

    added test images

--- src/Components/RetouchingExample/RetouchingExample.jsx
+++ src/Components/RetouchingExample/RetouchingExample.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import before from '../../assets/images/floro-cristina-5107-before.jpg';
+import after from '../../assets/images/floro-cristina-5107-final.jpg';
 
 export default function RetouchingExample() {
+  
   return (
-    <div>
-      Test Data
+    <div className='image-container'>
+
+      <img className='retouching-before' src={before} alt=""/>
+      <img className='retouching-after' src={after} alt=""/>
+
     </div>
   )
 }
